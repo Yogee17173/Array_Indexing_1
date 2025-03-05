4b8cfba7a979f51eea6573de63b71a3c23619bd9
YOGEESH K P <yogeesh793@gmail.com>
2025-03-05 10:44:47 +0530

    Add files via upload

---
+++ Array_Indexing_1.py
@@ -0,0 +1,55 @@
+# Get the first element from the following array:
+import numpy as np
+arr = np.array([1,2,3,4,5])
+print(arr[0])
+
+#1
+import numpy as an
+ann=an.array([5,6,4,2])
+print(ann[0])
+
+#2
+import numpy as bn
+bnn=bn.array([11,12,13,15])
+print(bnn[0])
+
+#3
+import numpy as cn
+cnn=cn.array([6,3,5,4])
+print(cnn[0])
+
+#4
+import numpy as dn
+dnn=dn.array([2,3,4,5])
+print(dnn[0])
+
+#5
+import numpy as en
+enn=en.array([3,2,4])
+print(enn[0])
+
+#6
+import numpy as fn
+fnn=fn.array([12,13,45,56,86])
+print(fnn[0])
+
+#7
+import numpy as gn
+gnn=gn.array([1,2,3])
+print(gnn[0])
+
+#8
+import numpy as hn
+hnn=hn.array([3,2,5])
+print(hnn[0])
+
+#9
+import numpy as In
+inn = In.array([5,6,7])
+print(inn[0])
+
+#10
+import numpy as jn
+jnn=jn.array([2,3,4,5])
+print(jnn[0])
+
